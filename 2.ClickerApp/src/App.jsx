4c9d584b02966e3ca86fb2188a0c9166b7db30ae
abhicode95 <abhiharman2103@gmail.com>
2024-12-02 19:29:35 +0530

    Clicker App

---
+++ 2.ClickerApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Clicker from "./components/Clicker";
+
+function App() {
+  const [title] = useState("Clicker");
+
+  return (
+    <div className="App">
+      <Header title={title} />
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
